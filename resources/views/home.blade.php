256ef60dcb86d1ba16ee963d511159f4bf98bc87
Yves <ylegris94@gmail.com>
2017-06-01 12:38:23 -0400

    Ajout de la vue 'Accueil' et d'une vue parent pour le menu

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.header')
+@section('title', 'Accueil')
+
+@section('content')
+    <p>This is my body content.</p>
+    
+@endsection
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Logements Chicoutimi - @yield('title')</title>
+</head>
+<body>
+
+    @section('sidebar')
+        This is the master sidebar.
+    @show
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+</body>
+</html>
