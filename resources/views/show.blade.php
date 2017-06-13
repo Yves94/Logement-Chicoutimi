fd17b2260745c716f38e1748dad0470c99d6f757
Yves <ylegris94@gmail.com>
2017-06-12 23:49:06 -0400

    Ajout de la vue Show

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,12 +2,36 @@
 @section('title', 'Vendre')
 
 @section('style')
-    
+    img {
+        float: left;
+        margin-right: 20px;
+    }
+
+    .price {
+        float: right;
+        margin-top: -37px;
+        font-weight: bold;
+    }
+
+    .description {
+        height: 120px;
+    }
+
+    .description p {
+        text-align: justify;
+    }
 @endsection
 
 @section('content')
-    <h3>Page show</h3>
+    <img src="{{ $place->thumbnail }}">
 
-    <p>{{ $place->title }}</p>
+    <h3 class="title">{{ $place->title }}</h3>
+    <h3 class="price">{{ $place->price }} $</h3>
+    <hr>
+    <h4>Adresse : {{ $place->address }}, Chicoutimi</h4><br>
+    <div class="description">
+        <p>{{ $place->description }}</p>
+    </div>
+    <button class="btn btn-default">Contacter le vendeur</button>
 
 @endsection
